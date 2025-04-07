b7199b60cb9104c27ffda32e6741a82d87cb62b0
ninaaaa3 <168448346+ninaaaa3@users.noreply.github.com>
2025-04-07 18:03:51 -0400

    /time entrypoint ready

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import ntplib
+import datetime
+
+servidor_ntp = "ntp.shoa.cl"
+c = ntplib.NTPClient()
 
 app = FastAPI()
 
-
 @app.get("/")
 def hello():
-    return "hello world" 
+    return "/"
+
+@app.get("/time")
+def return_datetime():
+    respuesta = c.request(servidor_ntp)
+    fecha_actual = datetime.datetime.fromtimestamp(respuesta.tx_time)
+    return fecha_actual
